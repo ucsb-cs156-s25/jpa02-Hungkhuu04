0edc40e06d335f273eac370d913c1b8e968aa637
Hung Khuu <hungkhuu@ucsb.edu>
2025-04-09 10:43:38 -0700

    xy - updated with actual developer name

--- src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
@@ -39,11 +39,10 @@ public class Developer {
      */
     
     public static Team getTeam() {
-        // TODO: Change this to your team name
-        Team team = new Team("s25-xx");
+        Team team = new Team("s25-12");
         team.addMember("Alice");
         team.addMember("Bob");
-        team.addMember("Chris G.");
+        team.addMember("Hung K");
         team.addMember("Danny");
         team.addMember("Eve");
         team.addMember("Frances");
